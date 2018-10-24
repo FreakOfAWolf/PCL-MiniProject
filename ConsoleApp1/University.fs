bf1d7de1ea07c03b6af84ac239bd90cc8e76d200
Carlos C. Alberto <cca@tickcad.dk>
2018-10-24 18:11:23 +0200

    commit 1 -phase 1

--- ConsoleApp1/University.fs
+++ ConsoleApp1/University.fs
@@ -1,5 +1,7 @@
 ﻿module University
 
+open System.Linq.Expressions
+
 let scale = [1..10]
 
 type Department(name:string) =
@@ -17,43 +19,75 @@ type PTraits(name:string, scale) =
   member x.Name = name;
   member x.scale = scale;
 
-let Programming = new PTraits(name="Programming", scale = 10)
-let Mathematics = new PTraits(name="Mathematics", scale = 7)
-let Social = new PTraits(name="Social", scale = 8)
-let Research = new PTraits(name="Research", scale = 9)
-let Biology = new PTraits(name="Biology", scale = 4)
+let Programming = new PTraits(name="Programming", scale = 9)
+let Mathematics = new PTraits(name="Mathematics", scale = 9)
+let Social = new PTraits(name="Sociology", scale = 9)
+let Research = new PTraits(name="Research", scale = 6)
+let Biology = new PTraits(name="Biology", scale = 7)
 
-type NonGroupedStudent(name:string, age:int, EPoint:int, PTraits:PTraits) =
+type NonGroupedStudent(name:string, age:int, EPoint:int, PTraits:List<PTraits>) =
   member x.Name = name;
   member x.Age = age;
   member x.EPoint = EPoint;
   member x.PTraits = PTraits;
 
-let Steven = new NonGroupedStudent(name="Steven", age=22, EPoint=105, PTraits=Programming)
-let Iva = new NonGroupedStudent(name="Iva", age= 20, EPoint=100, PTraits=Mathematics)
-let Kusky = new NonGroupedStudent(name="Kusky", age=21, EPoint=110, PTraits=Social)
-let Willy = new NonGroupedStudent(name="Willy", age=24, EPoint=120, PTraits=Research)
-let John = new NonGroupedStudent(name="John", age=22, EPoint=110, PTraits=Biology)
+let Steven = new NonGroupedStudent(name="Steven", age=22, EPoint=105, PTraits=[Programming; Research])
+let Iva = new NonGroupedStudent(name="Iva", age= 20, EPoint=100, PTraits= [Mathematics;Social])
+let Kusky = new NonGroupedStudent(name="Kusky", age=21, EPoint=110, PTraits= [Research; Biology])
+let Willy = new NonGroupedStudent(name="Willy", age=24, EPoint=120, PTraits=[Programming; Biology])
+let John = new NonGroupedStudent(name="John", age=22, EPoint=110, PTraits=[Research; Social])
+ //   if student.PTraits.Name="Biology" && student.PTraits.scale>=9 then HealthCare
+    //elif student.PTraits.Name="Research" && student.PTraits.scale>=8 then Marketing
+ //   else NoDep
+let findDepartment (traits:PTraits):Department =
+        let result:Department=
+          match traits.Name  with
+                | "Biology" when traits.scale >= 9 ->HealthCare 
+                | "Programming" when traits.scale >= 7 ->IT
+                | "Mathematics" when traits.scale >= 7 ->Mechanical
+                | "Sociology" when traits.scale >= 7 ->SocialStudies
+                | "Research" when traits.scale >= 7->Marketing
+                | _ -> NoDep
+        result;;
+                //let result:Department =student.PTraits |> Seq.map(fun x ->(findDepartment(x)))
 
-let grouping (student : NonGroupedStudent) =
-  let result = 
-    if student.PTraits.Name="Biology" && student.PTraits.scale>=9 then "Res"
-    elif student.PTraits.Name="Research" && student.PTraits.scale>=8 then "Hdhasd"
-    else ":dufibs"
-  result;;
+let grouping(student : NonGroupedStudent):string= 
+    let mutable result1:string = ""
+    for x in student.PTraits do 
+            result1 <- x.Name+": "+ result1+" - " + findDepartment(x).Name
+    result1;;
+
+let test1 = grouping(Willy)
+let test2= grouping(John)
+let test3= grouping(Iva)
+let test4= grouping(Steven)
+let test5= grouping(Kusky)
 
 
-let printStudent = grouping(Iva)
+printfn "%s" test1
+printfn "%s" test2
+printfn "%s" test3
+printfn "%s" test4
+printfn "%s" test5
 
-type student(student:NonGroupedStudent) = 
-  member x.Name = student.Name
-  member x.Age = student.Age
-  member x.EPoint = student.EPoint
-  member x.PTraits = student.PTraits
-  override x.GetHashCode() = hash(student)
-  override x.Equals(a) = 
-    match a with 
-    | :? student as p -> (student.PTraits.Name, student.PTraits.scale) = (p.PTraits.Name, p.PTraits.scale)
-    | _ -> false
+let ePoints =5,10
 
-student(Steven) = student(Iva)
+type RealStudent(Name:string, Age:int, ePoints, Department:string)=
+    member x.Name = Name;
+    member x.Age= Age;
+    member x.EPoint = ePoints;
+    member x.Department= Department;
+
+let register(student:NonGroupedStudent):RealStudent =
+    let result =
+       if student.EPoint>=110 then 
+          let department =grouping(student);
+          RealStudent(Name=student.Name,Age=student.Age,ePoints=student.EPoint,Department=department)
+       else
+         RealStudent(Name="",Age=0,ePoints=0,Department="")
+    result;;
+
+
+let f = register(Steven)
+
+printf "%s" f.Name
